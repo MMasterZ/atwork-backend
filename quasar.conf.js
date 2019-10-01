192d3f754c3a39941f38e901492134b577516fc7
ZiaoJ <jmaibok.winner@gmail.com>
2019-10-01 17:11:17 +0700

    แก้ไข dialog

--- quasar.conf.js
+++ quasar.conf.js
@@ -65,7 +65,8 @@ module.exports = function (ctx) {
         "QTabPanel",
         "QCheckbox",
         "QPagination",
-        'QDialog'
+        'QDialog',
+        "QSeparator"
       ],
 
       directives: [
