451f969d5969f146fd37d307552aa82d835d8014
MASTERZ <soulhunz@hotmail.com>
2019-10-11 12:10:33 +0700

    Auto stash before merge of "master" and "origin/master"

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,181 +1,183 @@
 const routes = [{
-  path: "/",
-  component: () => import("pages/login.vue"),
-  name: "login"
-},
-{
-  path: "/welcomeback",
-  component: () => import("pages/welcomeback.vue"),
-  name: "welcomeback"
-},
-// logout
-{
-  path: "/logout",
-  component: () => import("pages/logout.vue"),
-  name: "logout"
-},
-//  expressionprint
-{
-  path: "/expression/print",
-  component: () => import("pages/expressionprint.vue"),
-  name: "expressionprint"
-},
-{
-  path: "/vocabulary/print",
-  component: () => import("pages/vocabularyprint.vue"),
-  name: "vocabularyprint"
-},
-{
-  path: "/",
-  component: () => import("layouts/MyLayout.vue"),
-  children: [{
-    path: "/welcome",
-    component: () => import("pages/Index.vue"),
-    name: "welcome"
-  },
-  // หน้า Admin
-  {
-    path: "/admin",
-    component: () => import("pages/adminmain.vue"),
-    name: "adminmain"
+    path: "/",
+    component: () => import("pages/login.vue"),
+    name: "login"
   },
   {
-    path: "/admin/add",
-    component: () => import("pages/adminadd.vue"),
-    name: "adminadd"
+    path: "/welcomeback",
+    component: () => import("pages/welcomeback.vue"),
+    name: "welcomeback"
+  },
+  // logout
+  {
+    path: "/logout",
+    component: () => import("pages/logout.vue"),
+    name: "logout"
+  },
+  //  expressionprint
+  {
+    path: "/expression/print",
+    component: () => import("pages/expressionprint.vue"),
+    name: "expressionprint"
   },
   {
-    path: "/admin/edit/:key",
-    component: () => import("pages/adminadd.vue"),
-    name: "adminedit"
+    path: "/vocabulary/print/:key",
+    component: () => import("pages/vocabularyprint.vue"),
+    name: "vocabularyprint"
   },
   {
-    path: "/user/edit",
-    component: () => import("pages/adminadd.vue"),
-    name: "useredit"
-  },
+    path: "/",
+    component: () => import("layouts/MyLayout.vue"),
+    children: [{
+        path: "/welcome",
+        component: () => import("pages/Index.vue"),
+        name: "welcome"
+      },
+      // หน้า Admin
+      {
+        path: "/admin",
+        component: () => import("pages/adminmain.vue"),
+        name: "adminmain"
+      },
+      {
+        path: "/admin/add",
+        component: () => import("pages/adminadd.vue"),
+        name: "adminadd"
+      },
+      // Print
+      {
+        name: "dialogprint"
+        path: "/admin/edit/:key",
+        component: () => import("pages/adminadd.vue"),
+        name: "adminedit"
+      },
+      {
+        path: "/user/edit",
+        component: () => import("pages/adminadd.vue"),
+        name: "useredit"
+      },
 
-  // pages workplace
-  {
-    path: "/workplace",
-    component: () => import("pages/workplacemain.vue"),
-    name: "workplacemain"
-  },
-  {
-    path: "/workplace/add",
-    component: () => import("pages/workplaceadd.vue"),
-    name: "workplaceadd"
-  },
-  {
-    path: "/workplace/edit/:key",
-    component: () => import("pages/workplaceadd.vue"),
-    name: "workplaceedit"
-  },
+      // pages workplace
+      {
+        path: "/workplace",
+        component: () => import("pages/workplacemain.vue"),
+        name: "workplacemain"
+      },
+      {
+        path: "/workplace/add",
+        component: () => import("pages/workplaceadd.vue"),
+        name: "workplaceadd"
+      },
+      {
+        path: "/workplace/edit/:key",
+        component: () => import("pages/workplaceadd.vue"),
+        name: "workplaceedit"
+      },
 
-  // page Account
-  {
-    path: "/account",
-    component: () => import("pages/accountmain.vue"),
-    name: "accountmain"
-  },
-  {
-    path: "/account/add/:businesskey",
-    component: () => import("pages/accountadd.vue"),
-    name: "accountadd"
-  },
-  {
-    path: "/account/edit/:userkey/:businesskey?",
-    component: () => import("pages/accountadd.vue"),
-    name: "accountedit"
-  },
+      // page Account
+      {
+        path: "/account",
+        component: () => import("pages/accountmain.vue"),
+        name: "accountmain"
+      },
+      {
+        path: "/account/add/:businesskey",
+        component: () => import("pages/accountadd.vue"),
+        name: "accountadd"
+      },
+      {
+        path: "/account/edit/:userkey/:businesskey?",
+        component: () => import("pages/accountadd.vue"),
+        name: "accountedit"
+      },
 
-  // pages position
-  {
-    path: "/lesson",
-    component: () => import("pages/positionmain.vue"),
-    name: "positionmain"
-  },
-  {
-    path: "/lesson/add",
-    component: () => import("pages/positionadd.vue"),
-    name: "positionadd"
-  },
-  {
-    path: "/lesson/edit/:key",
-    component: () => import("pages/positionadd.vue"),
-    name: "positionedit"
-  },
+      // pages position
+      {
+        path: "/lesson",
+        component: () => import("pages/positionmain.vue"),
+        name: "positionmain"
+      },
+      {
+        path: "/lesson/add",
+        component: () => import("pages/positionadd.vue"),
+        name: "positionadd"
+      },
+      {
+        path: "/lesson/edit/:key",
+        component: () => import("pages/positionadd.vue"),
+        name: "positionedit"
+      },
 
-  // pages vocabulary
-  {
-    path: "/vocabulary",
-    component: () => import("pages/vocabularymain.vue"),
-    name: "vocabularymain"
-  },
-  {
-    path: "/vocabulary/add",
-    component: () => import("pages/vocabularyadd.vue"),
-    name: "vocabularyadd"
-  },
-  {
-    path: "/vocabulary/edit/:key",
-    component: () => import("pages/vocabularyadd.vue"),
-    name: "vocabularyedit"
-  },
+      // pages vocabulary
+      {
+        path: "/vocabulary",
+        component: () => import("pages/vocabularymain.vue"),
+        name: "vocabularymain"
+      },
+      {
+        path: "/vocabulary/add",
+        component: () => import("pages/vocabularyadd.vue"),
+        name: "vocabularyadd"
+      },
+      {
+        path: "/vocabulary/edit/:key",
+        component: () => import("pages/vocabularyadd.vue"),
+        name: "vocabularyedit"
+      },
 
 
-  // pages expression
-  {
-    path: "/expression",
-    component: () => import("pages/expressionmain.vue"),
-    name: "expressionmain"
-  },
-  {
-    path: "/expression/add",
-    component: () => import("pages/expressionadd.vue"),
-    name: "expressionadd"
-  },
-  {
-    path: "/expression/edit/:key",
-    component: () => import("pages/expressionadd.vue"),
-    name: "expressionedit"
-  },
+      // pages expression
+      {
+        path: "/expression",
+        component: () => import("pages/expressionmain.vue"),
+        name: "expressionmain"
+      },
+      {
+        path: "/expression/add",
+        component: () => import("pages/expressionadd.vue"),
+        name: "expressionadd"
+      },
+      {
+        path: "/expression/edit/:key",
+        component: () => import("pages/expressionadd.vue"),
+        name: "expressionedit"
+      },
 
-  // pages reading
-  {
-    path: "/dialog",
-    component: () => import("pages/dialogmain.vue"),
-    name: "dialogmain"
-  },
-  {
-    path: "/dialog/add",
-    component: () => import("pages/dialogadd.vue"),
-    name: "dialogadd"
-  },
-  {
-    path: "/dialog/edit/:key/:page",
-    component: () => import("pages/dialogadd.vue"),
-    name: "dialogedit"
-  },
+      // pages reading
+      {
+        path: "/dialog",
+        component: () => import("pages/dialogmain.vue"),
+        name: "dialogmain"
+      },
+      {
+        path: "/dialog/add",
+        component: () => import("pages/dialogadd.vue"),
+        name: "dialogadd"
+      },
+      {
+        path: "/dialog/edit/:key/:page",
+        component: () => import("pages/dialogadd.vue"),
+        name: "dialogedit"
+      },
 
-  // pages writing
-  {
-    path: "/writing",
-    component: () => import("pages/writingmain.vue"),
-    name: "writingmain"
-  },
-  {
-    path: "/writing/add",
-    component: () => import("pages/writingadd.vue"),
-    name: "writingadd"
-  },
-  {
-    path: "/landing/:key/:page",
-    component: () => import("pages/landing.vue"),
-    name: "landingpage"
+      // pages writing
+      {
+        path: "/writing",
+        component: () => import("pages/writingmain.vue"),
+        name: "writingmain"
+      },
+      {
+        path: "/writing/add",
+        component: () => import("pages/writingadd.vue"),
+        name: "writingadd"
+      },
+      {
+        path: "/landing/:key/:page",
+        component: () => import("pages/landing.vue"),
+        name: "landingpage"
+      }
+    ]
   }
-  ]
-}
 ];
 
 // Always leave this as last one
